1bb39868b949c5e753471a0d0bee9331bd7b0a6a
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2014-12-13 16:18:34 +0100

    Added activerecord integration tests

--- spec/integration/models/report_spec.rb
+++ spec/integration/models/report_spec.rb
@@ -36,3 +36,36 @@ describe Report, ".create_from_yaml" do
     end
   end
 end
+
+describe Report, '.get_with_log_entries' do
+  let(:yaml) { File.read(File.expand_path("../../fixtures/changed-notify.yaml", __FILE__)) }
+  let(:id)   { Report.create_from_yaml(yaml).id }
+
+  context 'fetching an existing report' do
+    subject { Report.get_with_log_entries id }
+    
+    it "should return value that isn't nil" do
+      subject.should_not be_nil
+    end
+    it "should return a report object" do
+      subject.class.should eq(Report)
+    end
+    it "should return persisted report" do
+      subject.persisted?.should be_truthy
+    end
+    it "should have status 'changed'" do
+      subject.status.should eq('changed')
+    end
+    it "should have 3 log entries, also persisted" do
+      subject.log_entries.size.should eq(3)
+      subject.log_entries.each do |log|
+        log.persisted?.should be_truthy
+      end
+    end
+    it "should have parent node, that is persistedand has status 'changed'" do
+      subject.node.should_not be_nil
+      subject.node.status.should eq('changed')
+      subject.node.persisted?.should be_truthy
+    end
+  end
+end
