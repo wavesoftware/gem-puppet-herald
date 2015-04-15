8b502cf541c0e3aa43b2d1e78158bda754acd4a3
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2015-04-16 00:42:05 +0200

    Next development version v0.9.0.pre

--- lib/puppet-herald/version.rb
+++ lib/puppet-herald/version.rb
@@ -14,7 +14,7 @@ module PuppetHerald
   end
 
   # Version for Herald
-  VERSION     = version_prep '0.8.0'
+  VERSION     = version_prep '0.9.0.pre'
   # Lincense for Herald
   LICENSE     = 'Apache-2.0'
   # Project name
